aee6aae350693cf2c99be1046a812ffc65ff88c7
MuindeEsther <estherndunge2000@gmail.com>
2025-09-12 11:55:35 +0300

    Basic Operations Tasks

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,14 @@
+# Assign specific values to two variables, number1 and number2.
+number1 = 15
+number2 = 25
+
+# Perform addition, subtraction, and multiplication on these numbers.
+number1 + number2
+number1 * number2
+number2 - number1
+
+# Print the results of these operations in a human-readable format.
+
+print("The sum of", number1, "and", number2, "is", number1 + number2)
+print("The product of", number1, "and", number2, "is", number1 * number2)
+print("The subtraction of",number1, "and", number2,"is", number2 - number1)
