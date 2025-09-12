c2c2b93ba571bb7b17d4797286fbc3f1c2956bdc
MuindeEsther <estherndunge2000@gmail.com>
2025-09-12 14:14:30 +0300

    Simple Interest

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,10 +1,10 @@
 # Define three variables in this file:
 # principal, rate, and time.
-principal = 1000  # The initial amount of money
-rate = 0.05         # The annual interest rate (in percentage)
-time = 3         # The time the money is invested for (in years)
+P = 1000  # The initial amount of money
+R = 0.05         # The annual interest rate (in percentage)
+T = 3         # The time the money is invested for (in years)
 
 # Calculate the simple interest using the formula:
-# Simple Interest = (Principal * Rate * Time)
-interest = (principal * rate * time)
+# interest = (principal * rate * time)
+interest = (P * R * T)
 print("The simple interest is:", interest)
