38641900c2355f3acfe82fade442c0589d46507d
MuindeEsther <estherndunge2000@gmail.com>
2025-09-12 14:04:14 +0300

    Modified Simple Interest

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -5,6 +5,6 @@ rate = 0.05         # The annual interest rate (in percentage)
 time = 3         # The time the money is invested for (in years)
 
 # Calculate the simple interest using the formula:
-# Simple Interest = (Principal * Rate * Time) / 100
-simple_interest = (principal * rate * time)
-print("The simple interest is:", simple_interest)
+# Simple Interest = (Principal * Rate * Time)
+interest = (principal * rate * time)
+print("The simple interest is:", interest)
