af1afb647a76d52648fe9aae90e6181a814cf7f8
MuindeEsther <estherndunge2000@gmail.com>
2025-09-18 12:01:13 +0300

    Simple Calculator with Match Case

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,7 +1,7 @@
 # Simple Calculator with Match Case
 
-num1 = int(input("Enter first number:"))
-num2 = int(input("Enter second number: "))
+num1 = int(input("Enter the first number:"))
+num2 = int(input("Enter the second number: "))
 
 operation = input("Choose an operation (+, -, *, /): ")
 
