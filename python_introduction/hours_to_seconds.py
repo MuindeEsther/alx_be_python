b3643349ea0a2023008fa5d786c73eaf8a29c669
MuindeEsther <estherndunge2000@gmail.com>
2025-09-18 10:00:07 +0300

    Weather Recommendation

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -3,4 +3,4 @@ hours = 2  # You can change this value to any number of hours
 # There are 3600 seconds in an hour (60 seconds/minute * 60 minutes/hour)
 seconds = hours * 3600
 # print the result in [hours] hour(s) is [seconds] seconds.
-print(f"{hours} hours is  {seconds} seconds.")
+print(f"{hours} hours is {seconds} seconds.")
